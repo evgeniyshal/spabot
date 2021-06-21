a096f56ec51f1285a4d13715d6e5dac4dffc8f13
evgen <evgeniyshal@gmail.com>
2021-06-21 14:22:14 +0300

    src /main & pom.xml

---
+++ src/main/java/com/worldofmassage/spabot/SpabotApplication.java
@@ -0,0 +1,13 @@
+package com.worldofmassage.spabot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpabotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpabotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/worldofmassage/spabot/SpabotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.worldofmassage.spabot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpabotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
