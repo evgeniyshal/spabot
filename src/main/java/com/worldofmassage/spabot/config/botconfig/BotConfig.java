ce5f360b535ac7ecc4e7e2508c0d4456ccc57f9e
evgen <evgeniyshal@gmail.com>
2021-06-22 14:20:23 +0300

    добавил ботКонфиг

--- src/main/java/com/worldofmassage/spabot/config/botconfig/BotConfig.java
+++ src/main/java/com/worldofmassage/spabot/config/botconfig/BotConfig.java
@@ -1,7 +1,13 @@
 package com.worldofmassage.spabot.config.botconfig;
 
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
+@Configuration
+@Data
+@PropertySource("classpath:application.properties")
 public class BotConfig {
 
 
