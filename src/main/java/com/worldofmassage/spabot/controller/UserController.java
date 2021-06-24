c452e884e21db3273be3aa18e5422aef370cee79
Dreder <dreder@icloud.com>
2021-06-24 19:50:45 +0300

    index fixes

--- src/main/java/com/worldofmassage/spabot/controller/UserController.java
+++ src/main/java/com/worldofmassage/spabot/controller/UserController.java
@@ -1,27 +1,20 @@
 package com.worldofmassage.spabot.controller;
 
-import com.worldofmassage.spabot.entity.User;
 import com.worldofmassage.spabot.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
   private final UserService userService;
 
   @Autowired
-    public UserController(UserService userService) {
-        this.userService = userService;
+  public UserController(UserService userService) {
+      this.userService = userService;
     }
 
-    @RequestMapping(value = "/registration", method = RequestMethod.GET)
-    public String registrationForm(User user){
-        return "registration";
-    }
-
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    @GetMapping(value = "/login")
     public String loginForm(){
         return "login";
     }
